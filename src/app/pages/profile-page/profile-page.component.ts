895acffddc7d7b1a94262b35a1f017ed4a745811
Kody Kapetanis <kodykapetanis@yahoo.com>
2025-03-10 21:05:47 -0400

    implement ui for profile

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MATERIAL_IMPORTS } from '../../material.imports';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [],
+  imports: [MATERIAL_IMPORTS],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.css'
 })
 export class ProfilePageComponent {
-
+  //input into the component, from the parent, similar to React Props
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+  deleteAccount() {}
+  saveProfileChanges(){}
 }
